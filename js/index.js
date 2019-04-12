f526aabdfc207bfdd230afeec31e5f4d751b75a5
Mark071198 <36264025+Mark071198@users.noreply.github.com>
2019-04-12 19:22:13 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var Game = function(options) {
+  this.bubbles = options.bubbles;
+  this.bubblesArr = [];
+  this.score = 0;
+  this.duration = options.duration;
+  
+  this.start = function() {
+    
+    setTimeout(function() {
+      this.finish();
+    }, this.duration);
